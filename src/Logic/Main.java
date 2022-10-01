efde146225c450e6d47bedd997e64649ab20e7cc
Martin <metaway008@gmail.com>
2022-10-01 16:02:32 -0500

    se terminan e instancian las clases y los threads

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,7 +1,17 @@
 package Logic;
 
+import data.Salon1;
+import data.Salon2;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Salon2 salon2 = new Salon2(10);
+        Salon1 salon1 = new Salon1(11);
+        Thread thread_salon2 = new Thread(salon2);
+        Thread thread_salon1 = new Thread(salon1);
+
+        thread_salon2.start();
+        thread_salon1.start();
+
     }
 }
--- src/data/Salon1.java
+++ src/data/Salon1.java
@@ -1,9 +1,41 @@
 package data;
 
+import java.util.Random;
+
 public class Salon1 implements Runnable{
+    Random ram = new Random();
+
+    private int duracion_clase;
+
+    public Salon1(int duracion_clase){
+        this.duracion_clase=duracion_clase;
+    }
+
+    public void stop(){
+        duracion_clase=-1;
+    }
 
     @Override
     public void run(){
+
+        int contador =0;
+        int numero_estudiantes=0;
+        int profesor = 1;
+
+        while(contador<this.duracion_clase){
+            try{
+                int numero_random = ram.nextInt(1,5);
+                contador++;
+                numero_estudiantes += numero_random;
+                Thread.sleep(1000);
+                System.out.println("Salon 1: "+contador+" minutos de clase, "+numero_estudiantes+" estudiantes y "+profesor+" profesores");
+            }
+            catch (InterruptedException error){
+                error.printStackTrace();
+            }
+        }
+        System.out.println("El salon 1 ha terminado su clase a los "+contador+" minutos, con "+numero_estudiantes+" estudiantes"+
+                " y "+profesor+" profesores");
 
     }
 }
